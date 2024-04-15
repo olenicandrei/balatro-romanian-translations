6cb84c8bba4e99174d6be7c92345047b7657eaa0
olenicandrei <olenicandrei@gmail.com>
2024-04-15 22:26:24 +0300

    restructured mod files

---
+++ BalatRO.bat
@@ -0,0 +1,46 @@
+:: Traducere în română pentru Balatro
+::
+:: Script de instalare pentru pachetul de limbă RO pentru Balatro
+:: Toate sursele actualizate sunt disponibile aici: https://github.com/olenicandrei/balatro-romanian-translations/
+::
+
+
+@echo off
+setlocal enabledelayedexpansion
+
+set "colorReset=[0m"
+set "resourcesFolder=Resurse_Localizare_Balatro"
+
+echo ==========================================
+echo ==  Traducere în română pentru Balatro  ==
+echo ==  Instalarea pachetului de limbă RO   ==
+echo ==========================================
+
+:: Definire folder
+set "RESOURCES=%~dp0BalatRO"
+mkdir "%RESOURCES%" 2>nul
+mkdir "%RESOURCES%\resources\textures\1x" 2>nul
+mkdir "%RESOURCES%\resources\textures\2x" 2>nul
+mkdir "%RESOURCES%\localization" 2>nul
+
+:: Definirea URL-ului de bază al repository-ului
+set "REPO_URL=https://raw.githubusercontent.com/olenicandrei/balatro-romanian-translations/main/"
+
+:: Se descarcă fișierele specifice în structura corectă
+echo Se descarcă resursele actualizate...
+set "files=boosters.png Tarots.png Vouchers.png icons.png Jokers.png ShopSignAnimation.png"
+for %%f in (%files%) do (
+    powershell -command "Invoke-WebRequest -Uri '%REPO_URL%/assets/1x/%%f' -OutFile '%RESOURCES%\resources\textures\1x\%%f'"
+    powershell -command "Invoke-WebRequest -Uri '%REPO_URL%/assets/2x/%%f' -OutFile '%RESOURCES%\resources\textures\2x\%%f'"
+)
+
+:: Se descarcă game.lua și ro.lua
+powershell -command "Invoke-WebRequest -Uri '%REPO_URL%/game.lua' -OutFile '%RESOURCES%\game.lua'"
+powershell -command "Invoke-WebRequest -Uri '%REPO_URL%/ro.lua' -OutFile '%RESOURCES%\localization\ro.lua'"
+
+
+:fin
+echo.
+echo.
+echo.
+local end
